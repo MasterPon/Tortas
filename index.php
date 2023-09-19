e958f924fe2241ffab08aefa43dc0d0fa3567501
MasterPon <masterpon8@gmail.com>
2023-09-19 10:06:56 -0600

    subir a github

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300&family=Maven+Pro&family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <title>Super Tortas</title>
+</head>
+
+<body>
+    <div class="titulo" id="titulo">
+    <h1>SUPER TORTAS</h1>
+    <hr class="titulo-hr" >
+    
+    </div>
+<br><br>
+    
+    <a href="uber.php">
+   <center>
+    <div class="uber" id="uber">
+        <img src="../TORTAS/imagenes/uber.png" alt="PEDIDO UBER">
+        UBER
+    </div></a>
+    <a href="transferencias.php  ">
+    <div class="transfer" id="transfer">
+        <img src="../TORTAS/imagenes/transferencia.png" alt="TRANSFERENCIA">
+        TRANSFERENCIA
+    </div></a>
+    <a href="index.php">
+        <div class="tortas" id="tortas">
+<img src="../TORTAS/imagenes/tortas.jpg" alt="Tortas">
+TORTAS
+
+        </div>
+    </a>
+    <a href="index.php">
+        <div class="refrescos" id="refrescos">
+       <img src="../TORTAS/imagenes//cocacola-logo.jpg" alt="Refrescos">
+       REFRESCOS
+        </div>
+    </a>
+    <a href="index.php">
+        <div class="corte" id="corte">
+            <img src="../TORTAS/imagenes/corte.png" alt="Corte de caja">
+            CORTE
+        </div>
+    </a></center>
+</body>
+</html> 
